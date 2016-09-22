874f15095146720c29adb9c748a17716d1ed27bd
guozengtao <m13522335765@163.com>
2016-09-22 17:31:57 +0800

    update

--- src/main/java/com/atguigu/test/Test.java
+++ src/main/java/com/atguigu/test/Test.java
@@ -1,8 +1,21 @@
 package com.atguigu.test;
 
+import org.apache.log4j.Logger;
+
 public class Test {
+	private static final Logger logger = Logger.getLogger(Test.class);
+	
 	public static void main(String[] args) {
-		System.out.println("Test git");
-		System.out.println("from github");
+		try{
+			logger.debug("begin");
+			int age = 10 /2;
+			if (logger.isInfoEnabled()) {
+				logger.info("main(String[]) - int age=" + age);
+			}
+		}catch(Exception e){
+			logger.error(e.getCause(), e);
+		}finally{
+			logger.info("end");
+		}
 	}
 }
